e6ac1b1e785caef8b0f805a38616ca9c6efedc3d
mohanru99 <131185988+mohanru99@users.noreply.github.com>
2024-03-09 21:51:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
-const formOpenBtn = document.querySelector("#form-open"),
-  home = document.querySelector(".home"),
-  formContainer = document.querySelector(".form_container"),
-  formCloseBtn = document.querySelector(".form_close"),
-  signupBtn = document.querySelector("#signup"),
-  loginBtn = document.querySelector("#login"),
-  pwShowHide = document.querySelectorAll(".pw_hide");
+const formOpenBtn = document.querySelector("#form-open");
+const home = document.querySelector(".home");
+const formContainer = document.querySelector(".form_container");
+const formCloseBtn = document.querySelector(".form_close");
+const signupBtn = document.querySelector("#signup");
+const loginBtn = document.querySelector("#login");
+const pwShowHide = document.querySelectorAll(".pw_hide");
 
 formOpenBtn.addEventListener("click", () => home.classList.add("show"));
 formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
 pwShowHide.forEach((icon) => {
   icon.addEventListener("click", () => {
-    let getPwInput = icon.parentElement.querySelector("input");
-    if (getPwInput.type === "password") {
-      getPwInput.type = "text";
-      icon.classList.replace("uil-eye-slash", "uil-eye");
-    } else {
-      getPwInput.type = "password";
-      icon.classList.replace("uil-eye", "uil-eye-slash");
-    }
+    const getPwInput = icon.parentElement.querySelector("input");
+    getPwInput.type = getPwInput.type === "password" ? "text" : "password";
+    const iconClass = getPwInput.type === "password" ? "uil-eye" : "uil-eye-slash";
+    icon.classList.replace("uil-eye-slash", iconClass);
   });
 });
 
@@ -26,6 +22,7 @@ signupBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.add("active");
 });
+
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
   formContainer.classList.remove("active");
